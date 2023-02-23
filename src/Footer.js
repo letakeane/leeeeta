142ce0541a99e8a727266453aa69381d31ea5c14
Leta Keane <leta@hey.com>
2023-02-22 23:36:06 -0700

    Add active states for navlinks

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,34 @@ const Footer = () => {
   return (
     <footer className="footer">
       <section className="links">
-        <NavLink exact to="/">home</NavLink>
-        <NavLink to="/about">about</NavLink>
-        <NavLink to="/work">work</NavLink>
-        <NavLink to="/contact">contact</NavLink>
+        <NavLink exact to="/" className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }>home</NavLink>
+        <NavLink to="/about" className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }>about</NavLink>
+        <NavLink to="/work" className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }>work</NavLink>
+        <NavLink to="/contact" className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }>contact</NavLink>
       </section>
     </footer>
   )
